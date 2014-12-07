dab070d33f4963c80944dc4c65cbb34878da2ad8
Sergii Kozlov <derzaicheg@gmail.com>
2014-12-07 14:20:02 +0100

    added test structure for the future test suites and test cases. It is possible to move them to src folder but it could be better to keep them separately

---
+++ tests/src/testPlan/01-testSuiteGet/go_test.go
@@ -0,0 +1,11 @@
+package gotest1
+
+import (
+	
+	"testing"
+	"fmt"
+)
+
+func Test1(t *testing.T) {
+	fmt.Println("Hello 1")
+}
---
+++ tests/src/testPlan/02-testSuitePost/go_test.go
@@ -0,0 +1,12 @@
+package gotest1
+
+import (
+	
+	"testing"
+	"fmt"
+)
+
+func Test1(t *testing.T) {
+	fmt.Println("Hello 2")
+	
+}
